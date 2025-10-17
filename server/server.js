182b29f18df6d0272310ece3b19293be61ada6bf
Alex Rivard <alex.rivard4406@gmail.com>
2025-10-17 14:18:13 -0500

    made it so no one should be able to find our site through a search engine

--- server/server.js
+++ server/server.js
@@ -1,8 +1,23 @@
-// Dependancies
+// Dependencies
 const express = require("express");
 const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT || 5001;
+
+// --- No-index / nofollow for all responses ---
+app.use((req, res, next) => {
+  // Helps keep all content out of search results (covers non-HTML too)
+  res.setHeader("X-Robots-Tag", "noindex, nofollow");
+  next();
+});
+
+// --- robots.txt that disallows everything ---
+// Put this BEFORE express.static so it always wins.
+app.get("../robots.txt", (_req, res) => {
+  res
+    .type("text/plain")
+    .send("User-agent: *\nDisallow: /");
+});
 
 // Route Includes
 const test = require("./routes/test.router");
@@ -11,6 +26,8 @@ const sheets = require("./routes/googlesheets.router");
 // Express Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Static files (after robots.txt so our custom one wins)
 app.use(express.static("build"));
 
 // Routes
